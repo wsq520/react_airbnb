957e43a2f4815d2ad741c51b7c1b61a62c54546d
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2023-05-04 16:19:49 +0800

    右上角弹窗显示和隐藏

--- src/components/app-header/cpns/header-right/index.jsx
+++ src/components/app-header/cpns/header-right/index.jsx
@@ -3,8 +3,26 @@ import { RightWrapper } from './style'
 import IconGlobal from '@/assets/svg/icon_global'
 import IconMenu from '@/assets/svg/icon_menu'
 import IconAvatar from '@/assets/svg/icon_avatar'
+import { useState, useEffect } from 'react'
 
 const HeaderRight = memo(() => {
+  const [showPanel, setShowPanel] = useState(false)
+
+  function profileCickHandle() {
+    setShowPanel(true)
+  }
+
+  useEffect(() => {
+    function windowHandleClick() {
+      setShowPanel(false)
+    }
+    // 这里涉及到事件冒泡和捕获的问题 这里采用了事件捕获方法解决显示隐藏的问题
+    window.addEventListener("click", windowHandleClick, true)
+    return () => {
+      window.removeEventListener("click", windowHandleClick, true)
+    }
+  }, [])
+
   return (
     <RightWrapper>
       <div className='btns'>
@@ -14,9 +32,24 @@ const HeaderRight = memo(() => {
           <IconGlobal />
         </span>
       </div>
-      <div className='profile'>
+      <div className='profile' onClick={profileCickHandle}>
         <IconMenu />
         <IconAvatar />
+        {
+          showPanel && (
+            <div className='panel'>
+              <div className='top'>
+                <div className='item register'>注册</div>
+                <div className='item login'>登录</div>
+              </div>
+              <div className='bottom'>
+                <div className='item'>出租房源</div>
+                <div className='item'>开展体验</div>
+                <div className='item'>帮助</div>
+              </div>
+            </div>
+          )
+        }
       </div>
     </RightWrapper>
   )
