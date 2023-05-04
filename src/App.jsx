22b5c7bc77f3021d4f2da94922a99ffaf8ba7295
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2023-05-04 12:54:59 +0800

    配置craco

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
 import React, { memo } from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
 
 const App = memo((props) => {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    console.log(111)
-
-    return () => {
-      console.log(2222)
-    }
-  }, [])
-
   return (
     <div>
-      <h2>App</h2>
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      App
     </div>
   )
 })
